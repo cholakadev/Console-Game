5cd77f096fea740671f9de18fed67e8e7d3a2c91
cholakovge <cholakovge@gmail.com>
2019-11-23 01:07:14 +0200

    Monster class

---
+++ Monster.cs
@@ -0,0 +1,24 @@
+﻿namespace Game
+{
+    using System;
+    public class Monster
+    {
+        public Monster()
+        {
+            this.Name = "Goblin";
+            this.Damage = 0;
+            this.Health = Random(30, 40);
+        }
+        public string Name { get; set; }
+
+        public int Damage { get; set; }
+
+        public int Health { get; set; }
+
+        public int Random(int minValue, int maxValue)
+        {
+            Random random = new Random();
+            return random.Next(minValue, maxValue);
+        }
+    }
+}
