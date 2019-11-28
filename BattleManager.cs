ebbde9deae2c936eeb9a21367f2f96f50bbdc610
cholakovge <cholakovge@gmail.com>
2019-11-28 19:50:37 +0200

    Class Methods separated to specific methods.

---
+++ BattleManager.cs
@@ -0,0 +1,30 @@
+﻿namespace Game
+{
+    using System;
+    public class BattleManager
+    {
+        public static void PrintFightResults(Player pl, Monster monster)
+        {
+            if (pl.Health <= monster.Health)
+            {
+                Console.WriteLine($"{monster.Name} has slain {pl.Name} and left with {monster.Health} health!");
+            }
+            else if (monster.Health <= pl.Health)
+            {
+                Console.WriteLine($"{pl.Name} has slain {monster.Name} and left with {pl.Health} health!");
+            }
+        }
+
+        public static void Fight(Player pl, Monster monster)
+        {
+            pl.Damage = pl.Random(5, 12);
+            monster.Damage = monster.Random(5, 12);
+
+            pl.DamageAbsorb = pl.Random(1, 4);
+
+            pl.Health -= monster.Damage - pl.DamageAbsorb;
+
+            monster.Health -= pl.Damage;
+        }
+    }
+}
---
+++ LootManager.cs
@@ -0,0 +1,26 @@
+﻿namespace Game
+{
+    using System;
+    public class LootManager
+    {
+        public static void DropSilver(Player pl)
+        {
+            Random random = new Random();
+            int result = random.Next(55, 85);
+            pl.Silver += result;
+
+            if (pl.Silver >= 100)
+            {
+                pl.Silver -= 100;
+                pl.Gold++;
+                Console.WriteLine(pl.Gold + " gold.");
+            }
+        }
+
+        public static int Random(int minValue, int maxValue)
+        {
+            Random random = new Random();
+            return random.Next(minValue, maxValue);
+        }
+    }
+}
