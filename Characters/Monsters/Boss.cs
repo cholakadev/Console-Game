871116e4bd57f9fb3c16ac06bc05688a509f7871
cholakovge <cholakovge@gmail.com>
2020-02-24 19:32:06 +0200

    Boss class

--- Characters/Monsters/Boss.cs
+++ Characters/Monsters/Boss.cs
@@ -3,6 +3,13 @@
     using Game.Methods;
     public class Boss : IEnemy
     {
+        public Boss()
+        {
+            this.Name = "Goblin's Head";
+            this.Damage = 30;
+            this.Health = 100;
+        }
+
         public string Name { get; private set; }
 
         public int Damage { get; private set; }
